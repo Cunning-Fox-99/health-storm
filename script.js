ef5638de117d7772d567946a446314cc8c4da382
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2023-03-28 21:46:09 +0300

    fixes

--- script.js
+++ script.js
@@ -43,4 +43,19 @@ window.addEventListener('DOMContentLoaded', function() {
         button.classList.toggle('active')
     })
 
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            const blockID = anchor.getAttribute('href').substr(1)
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+
 });
